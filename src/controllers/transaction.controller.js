c65105d52e1ad639ffe6e59b1bb6de98e6cb03e4
Ajay Sood <ajays@brillersoft.com>
2020-05-23 17:21:33 +0530

    add api changes

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -15,21 +15,53 @@ const getTransactions = catchAsync(async (req, res) => {
 });
 
 const addTransactions = catchAsync(async (req, res) => {
-  const lastTransaction = await Transaction.findOne({}).sort({ createdOn: -1 });
+  let{transaction,amount,description}=req.body
+  const lastTransaction = await Transaction.find({}).sort({_id:-1}).limit(1);
+  console.log('====================================');
+  console.log({lastTransaction});
+  console.log('====================================');
   if (lastTransaction) {
-    let obj = { 
-        description: req.body.description||"alpha" ,
-        amount:req.body.amount||'',
-        transaction:req.body.transaction|| ''
+    let obj = {
+      description: description || 'alpha',
+      amount: amount || '',
+      transaction:transaction || '',
+
     };
+    if(req.body.transaction=='Credit' ) 
+    {
+        let previousRunningBalance=lastTransaction[0].runningBalance?lastTransaction[0].runningBalance:0
+      obj.credit=amount
+      obj.runningBalance=Number(previousRunningBalance)+Number(obj.credit)
+      console.log('===========previous=========================');
+      console.log({previousRunningBalance})
+      console.log('====================================');
+    }
+    else{
+      let previousRunningBalance=lastTransaction[0].runningBalance?lastTransaction[0].runningBalance:0
+      obj.debit=amount
+      obj.runningBalance=Number(previousRunningBalance)-Number(obj.debit)
+      console.log('===========previous=========================');
+      console.log({previousRunningBalance})
+      console.log('====================================');
+    }
+   
     let savedTransaction = await Transaction.create(obj);
     res.send(savedTransaction);
   } else {
-    let obj = { 
-        description: req.body.description||"alpha" ,
-        amount:req.body.amount||'',
-        transaction:req.body.transaction|| ''
+   
+    let obj = {
+      description: description || 'alpha',
+      amount: amount || '',
+      transaction:transaction || '',
+      runningBalance:amount 
     };
+    if(req.body.transaction=='Credit' ) 
+    {
+      obj.credit=amount
+    }
+    else{
+      obj.debit=amount
+    }
     let savedTransaction = await Transaction.create(obj);
     res.send(savedTransaction);
   }
